21913ba6146efe083d7c732fababf8d7ff71466e
Aissam <abouzama@almata.cat>
2024-11-27 19:07:37 +0100

    UF 3 - DOM

---
+++ documentacio-UF1-UF2-JS/UF3-DOM/js/script.js
@@ -0,0 +1,9 @@
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+console.log(document.documentElement);
+console.log(document.characterSet);
+console.log(document.title);
+console.log(document.links);
+console.log(document.styleSheets);
+console.log(document.scripts);
